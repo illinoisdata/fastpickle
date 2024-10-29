39c64112d3e131484fe7d64e419f1aa0d855b8a6
Supawit Chockchowwat <supawitch22@gmail.com>
2024-10-29 16:31:41 -0500

    Reorganize repository structure

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
-# run with python3.11
-# run using python3.11 setup.py build_ext --inplace
-# or pip3.11 install -e .
-from setuptools import setup, Extension
+from setuptools import Extension, setup
 
-pickle_module = Extension('_pickle',
-                          sources=['_pickle.c'],
-                          include_dirs=['Include/internal', 'Include'],
-                          extra_compile_args=['-fno-strict-overflow', '-Wsign-compare', '-Wunreachable-code', '-DNDEBUG', '-g', '-O3', '-Wall'],
-                          )
+
+# Modules to be compiled and include_dirs when necessary
+c_extensions = [
+    Extension(
+        '_pickle',
+        sources=['src/_pickle.c'],
+        include_dirs=['include/internal', 'include'],
+        extra_compile_args=[
+            '-fno-strict-overflow',
+            '-Wsign-compare',
+            '-Wunreachable-code',
+            '-DNDEBUG',
+            '-g',
+            '-O3',
+            '-Wall',
+        ],
+    )
+]
+
 
 setup(
-    name='faster_pickle',
-    version='1.0',
-    description='A pickle module extension',
-    ext_modules=[pickle_module],
-    py_modules=['pickle'],
+    ext_modules=[
+        # *c_extensions,  # TODO: Broken on LLVM
+    ],
 )
