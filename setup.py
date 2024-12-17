f8c458e1ed1aabc790c48ec8de894bea488d4723
Talika Gupta <talikacsg@gmail.com>
2024-12-17 11:35:24 +0530

    revert setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,27 @@
-# setup.py
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext as build_ext_orig
-import sys
+from setuptools import Extension, setup
 
-is_debug = True  
 
-compile_args = [
-    '-fno-strict-overflow',
-    '-Wsign-compare',
-    '-Wunreachable-code',
-    '-Wall',
-    '-lpthread',
-    '-fno-inline',         
-    '-fvisibility=default',
+# Modules to be compiled and include_dirs when necessary
+c_extensions = [
+    Extension(
+        '_fastpickle',
+        sources=['src/_fastpickle.c'],
+        include_dirs=['include/internal', 'include'],
+        extra_compile_args=[
+            '-fno-strict-overflow',
+            '-Wsign-compare',
+            '-Wunreachable-code',
+            '-DNDEBUG',
+            '-g',
+            '-O3',
+            '-Wall',
+        ],
+    )
 ]
-link_args = []
 
-if is_debug:
-    compile_args += ['-g', '-O0', '-fno-omit-frame-pointer']
-    # ['-g', '-O0', '-fno-omit-frame-pointer', '-fsanitize=address']
-    # Uncomment below if address sanitization is needed
-    # link_args += ['-fsanitize=address']
-else:
-    compile_args += ['-DNDEBUG', '-O3']
-
-pickle_module = Extension(
-    '_fastpickle',
-    sources=['src/_fastpickle.c'],
-    include_dirs=['Include/internal', 'Include/custom', 'Include'],
-    extra_compile_args=compile_args,
-    extra_link_args=link_args,
-)
 
 setup(
-    name='faster_pickle',
-    version='1.0',
-    description='A pickle module extension',
-    ext_modules=[pickle_module],
-    py_modules=['pickle'],
+    ext_modules=[
+        *c_extensions,  # TODO: Broken on LLVM
+    ],
 )
